3f15ed477b9f7e6376be2b7601ab8b3aed8b9785
chgwyellow <dinnis1107@gmail.com>
2025-05-11 13:49:36 +0800

    completed the whole project manually.

--- morse_code_converter.py
+++ morse_code_converter.py
@@ -57,14 +57,29 @@ while keep_going:
     input_msg_list = list(input_msg)
     output_list = []
 
+    previous_char = " "
     for char in input_msg_list:
         if char in morse_code_rules:
             # if the space is between two words.
             if char == " ":
                 output_list.append(morse_code_rules[char] * 7)
+                previous_char = char
                 continue
+            # if the previous char is original space, skip comparison.
+            elif previous_char == " ":
+                output_list.append(morse_code_rules[char])
+                previous_char = char
+                continue
+            # if the char is equal to the previous char
+            elif char == previous_char:
+                output_list.append(morse_code_rules[" "])
+            # if the char is not the previous char
+            elif char != previous_char:
+                output_list.append(morse_code_rules[" "] * 3)
+
             output_list.append(morse_code_rules[char])
+            previous_char = char
         else:
             print(f"There are illegal marks in the original input: {char}")
-            exit() # Force to leave the loop
+            exit()  # Force to leave the loop
     print(f"The Morse Code is: {output_list}\n")
